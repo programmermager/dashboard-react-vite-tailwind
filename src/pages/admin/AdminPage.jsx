4e7b9545291fee33dd92cbe02f2d9bfa04c1a863
muhwid <muhwid29@gmail.com>
2024-10-03 12:12:39 +0800

    revert rename file

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Users, ChevronDown } from "lucide-react";
-import { Logo } from "../../components/logo";
+import { Logo } from "../../components/Logo";
 import { replace, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import { useEffect } from "react";
